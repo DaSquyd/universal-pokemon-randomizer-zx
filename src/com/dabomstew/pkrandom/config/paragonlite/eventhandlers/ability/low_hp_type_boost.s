f3fc8e04d2d0053df93482c3fae6b658ec0ee9c8
DaSquyd <david.jacquish@septyr.com>
2024-12-03 02:14:37 -0800

    Update low_hp_type_boost.s

--- src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/low_hp_type_boost.s
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/low_hp_type_boost.s
@@ -10,17 +10,18 @@
     cmp     r5, r0
     bne     Return
     
+; Check move type
+    mov     r0, #VAR_MoveType ; move type
+    bl      Battle::EventVar_GetValue
+    cmp     r4, r0
+    bne     Return
+    
+; Get Target HP
     mov     r0, r6
     mov     r1, r5
     bl      Battle::GetPoke
     mov     r6, r0
-    
-; Get Target HP
-#if PARAGONLITE
-    mov     r1, #2 ; 1/2 HP
-#else
-    mov     r1, #3 ; 1/3 HP
-#endif
+    mov     r1, #(PARAGONLITE ? 2 : 3) ; 1/2 HP in ParagonLite, 1/3 HP otherwise
     bl      Battle::DivideMaxHP
     mov     r5, r0
     
@@ -31,24 +32,28 @@
     
 ; Branch if current hp > target
     cmp     r0, r5
-    bhi     Return
+#if REDUX
+    bls     ApplyFullRatio
     
-; Check move type
-    mov     r0, #VAR_MoveType ; move type
-    bl      Battle::EventVar_GetValue
-    cmp     r4, r0
-    bne     Return
-    
-    mov     r0, #VAR_Ratio
-#if PARAGONLITE
-    ldr     r1, =5325 ; 1.3x
-#elif REDUX
-    ldr     r1, =4915 ; 1.2x
+    ; in Redux, there's at least a 1.2x bonus
+    ldr     r1, =(0x1000 * 1.2)
+    b       MulValue
 #else
-    mov     r1, #6
-    lsl     r1, #10 ; 6144 (1.5x)
+    bhi     Return
 #endif
+    
+ApplyFullRatio:
+#if PARAGONLITE
+    ldr     r1, =(0x1000 * 1.3)
+#else
+    mov     r1, #(0x1800 >> 10) ; 1.5x
+    lsl     r1, #10
+#endif
+
+MulValue:
+    mov     r0, #VAR_Ratio
     bl      Battle::EventVar_MulValue
     
 Return:
     pop     {r3-r7, pc}
+    
