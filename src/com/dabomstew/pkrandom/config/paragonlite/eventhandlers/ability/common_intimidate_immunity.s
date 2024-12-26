43d2863a40997db63c921b86c126d30acd366d11
DaSquyd <david.jacquish@septyr.com>
2024-12-25 19:47:09 -0800

    fixed intimidate immunity crash

--- src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/common_intimidate_immunity.s
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/common_intimidate_immunity.s
@@ -1,6 +1,7 @@
-    push    {r3-r5, lr}
+    push    {r3-r6, lr}
     mov     r5, r1
     mov     r4, r2
+    mov     r6, r3
     
     mov     r0, #VAR_IntimidateFlag
     bl      Battle::EventVar_GetValue
@@ -9,15 +10,15 @@
     
     mov     r0, r5
     mov     r1, r4
-    mov     r2, r3
+    mov     r2, r6
     mov     r3, #STSG_Attack
     bl      Battle::CommonStatDropGuardCheck
     
     mov     r0, r5
     mov     r1, r4
-    mov     r2, r3
+    mov     r2, r6
     mov     r3, #BTLTXT_AttackNotLowered
     bl      Battle::CommonStatDropGuardMessage
     
 Return:
-    pop     {r3-r5, pc}
+    pop     {r3-r6, pc}
