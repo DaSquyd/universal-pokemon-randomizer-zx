948cb0eccf6e68c1077accf904fbe0590fa55125
DaSquyd <david.jacquish@septyr.com>
2025-06-26 23:14:43 -0700

    Create AbilityHackMod_158_Prankster_QuickGuardDarkType.java

---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/modern/AbilityHackMod_158_Prankster_QuickGuardDarkType.java
@@ -0,0 +1,25 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.modern;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+
+import java.util.List;
+
+public class AbilityHackMod_158_Prankster_QuickGuardDarkType extends AbilityHackMod {
+    public AbilityHackMod_158_Prankster_QuickGuardDarkType() {
+        super(Abilities.prankster);
+    }
+
+    @Override
+    public String getExplanation(Context context, List<String> allExplanations) {
+        // TODO
+        return super.getExplanation(context, allExplanations);
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        // TODO
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onGetMovePriority, "prankster.s"));
+    }
+}
