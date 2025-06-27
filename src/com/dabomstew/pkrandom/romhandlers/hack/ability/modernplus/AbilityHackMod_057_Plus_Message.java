7df18ecb0deb80c6d003efca41e451431fa7d888
DaSquyd <david.jacquish@septyr.com>
2025-06-26 23:14:52 -0700

    Create AbilityHackMod_057_Plus_Message.java

---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/modernplus/AbilityHackMod_057_Plus_Message.java
@@ -0,0 +1,25 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.modernplus;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+
+import java.util.List;
+
+public class AbilityHackMod_057_Plus_Message extends AbilityHackMod {
+    String message;
+
+    public AbilityHackMod_057_Plus_Message(String message) {
+        super(Abilities.plus);
+
+        this.message = message;
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onGetAttackingStatValue));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onSwitchIn, "plus_message.s"));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onRotateIn, "plus_message.s"));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onPostAbilityChange, "plus_message.s"));
+    }
+}
