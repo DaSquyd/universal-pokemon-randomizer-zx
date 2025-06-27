a27d4900c531673251239c51992e7236fbd031dc
DaSquyd <david.jacquish@septyr.com>
2025-06-26 23:14:49 -0700

    Create AbilityHackMod_045_SandStream_SandstormImmunity.java

---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/modernplus/AbilityHackMod_045_SandStream_SandstormImmunity.java
@@ -0,0 +1,20 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.modernplus;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+
+import java.util.List;
+
+public class AbilityHackMod_045_SandStream_SandstormImmunity extends AbilityHackMod {
+    public AbilityHackMod_045_SandStream_SandstormImmunity() {
+        super(Abilities.sandStream);
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onSwitchIn));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onPostAbilityChange));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onWeatherReaction, "sand_stream_no_damage.s"));
+    }
+}
