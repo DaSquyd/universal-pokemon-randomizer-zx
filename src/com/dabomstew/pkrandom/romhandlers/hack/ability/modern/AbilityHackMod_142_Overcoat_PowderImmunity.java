bbb1f2b242d0db23db12e4e5050ec40e27755f63
DaSquyd <david.jacquish@septyr.com>
2025-06-26 23:14:36 -0700

    overcoat

--- src/com/dabomstew/pkrandom/romhandlers/hack/ability/modern/AbilityHackMod_142_Overcoat_PowderImmunity.java
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/modern/AbilityHackMod_142_Overcoat_PowderImmunity.java
@@ -1,18 +1,20 @@
 package com.dabomstew.pkrandom.romhandlers.hack.ability.modern;
 
 import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
 import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
 
 import java.util.List;
 
-public class AbilityHackMod_142_Overcoat extends AbilityHackMod {
-    public AbilityHackMod_142_Overcoat() {
+public class AbilityHackMod_142_Overcoat_PowderImmunity extends AbilityHackMod {
+    public AbilityHackMod_142_Overcoat_PowderImmunity() {
         super(Abilities.overcoat);
     }
 
     @Override
     public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
-        // TODO
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onWeatherReaction));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onAbilityCheckNoEffect, "overcoat_powder_immunity.s"));
     }
 
     @Override
