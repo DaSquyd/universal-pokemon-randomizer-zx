135178bcd0fde8eb0177d7206009ce14aae11334
DaSquyd <david.jacquish@septyr.com>
2025-06-26 23:13:01 -0700

    rattled

---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/custom/AbilityHackMod_155_Rattled_IntimidateActivationAndBugGhostResist.java
@@ -0,0 +1,30 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.custom;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+
+import java.util.List;
+
+public class AbilityHackMod_155_Rattled_IntimidateActivationAndBugGhostResist extends AbilityHackMod {
+    public AbilityHackMod_155_Rattled_IntimidateActivationAndBugGhostResist() {
+        super(Abilities.rattled);
+    }
+
+    @Override
+    public String getExplanation(Context context, List<String> allExplanations) {
+        // TODO
+        return super.getExplanation(context, allExplanations);
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onGetAttackingStatValue, "rattled_resist_redux.s"));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onStatStageChangeSuccess, "rattled_on_intimidate.s"));
+    }
+
+    @Override
+    public Boolean isBreakable() {
+        return true;
+    }
+}
---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/modern/AbilityHackMod_155_Rattled_IntimidateActivation.java
@@ -0,0 +1,25 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.modern;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+
+import java.util.List;
+
+public class AbilityHackMod_155_Rattled_IntimidateActivation extends AbilityHackMod {
+    public AbilityHackMod_155_Rattled_IntimidateActivation() {
+        super(Abilities.rattled);
+    }
+
+    @Override
+    public String getExplanation(Context context, List<String> allExplanations) {
+        // TODO
+        return super.getExplanation(context, allExplanations);
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onMoveDamageReaction1));
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onStatStageChangeSuccess, "rattled_on_intimidate.s"));
+    }
+}
