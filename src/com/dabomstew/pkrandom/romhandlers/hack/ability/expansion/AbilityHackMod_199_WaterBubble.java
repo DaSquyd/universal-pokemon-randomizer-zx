526fe8a7a3fbab3151ec2e6b600f8c37ce4c484f
DaSquyd <david.jacquish@septyr.com>
2025-07-01 13:48:31 -0700

    Update AbilityHackMod_199_WaterBubble.java

--- src/com/dabomstew/pkrandom/romhandlers/hack/ability/expansion/AbilityHackMod_199_WaterBubble.java
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/expansion/AbilityHackMod_199_WaterBubble.java
@@ -11,6 +11,11 @@ public class AbilityHackMod_199_WaterBubble extends AbilityHackMod {
     }
 
     @Override
+    public String getName(Context context, List<String> allNames) {
+        return "Water Bubble";
+    }
+
+    @Override
     public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
         // TODO
     }
