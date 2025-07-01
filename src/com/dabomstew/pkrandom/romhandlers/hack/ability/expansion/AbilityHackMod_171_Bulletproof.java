0ff55185cf0ab4c3d73b3e69099e40cf068be378
DaSquyd <david.jacquish@septyr.com>
2025-07-01 13:47:58 -0700

    Update AbilityHackMod_171_Bulletproof.java

--- src/com/dabomstew/pkrandom/romhandlers/hack/ability/expansion/AbilityHackMod_171_Bulletproof.java
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/expansion/AbilityHackMod_171_Bulletproof.java
@@ -1,6 +1,7 @@
 package com.dabomstew.pkrandom.romhandlers.hack.ability.expansion;
 
 import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
 import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
 
 import java.util.List;
@@ -11,8 +12,24 @@ public class AbilityHackMod_171_Bulletproof extends AbilityHackMod {
     }
 
     @Override
-    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+    public String getName(Context context, List<String> allNames) {
+        return "Bulletproof";
+    }
+
+    @Override
+    public String getDescription(Context context, List<String> allDescriptions) {
+        return "Protects the Pokémon from\\xFFFEsome ball and bomb moves.";
+    }
+
+    @Override
+    public String getExplanation(Context context, List<String> allExplanations) {
         // TODO
+        return super.getExplanation(context, allExplanations);
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onAbilityCheckNoEffect, "bulletproof.s"));
     }
 
     @Override
