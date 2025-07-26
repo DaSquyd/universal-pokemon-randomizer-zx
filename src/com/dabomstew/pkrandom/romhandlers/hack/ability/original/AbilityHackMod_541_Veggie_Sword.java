a754ecf5d68635f842e0c89e6a0dd168fcbfc617
AphexCubed <therealnobodynoone@gmail.com>
2025-07-26 13:32:46 +0100

    Add Veggie Sword

---
+++ src/com/dabomstew/pkrandom/romhandlers/hack/ability/original/AbilityHackMod_541_Veggie_Sword.java
@@ -0,0 +1,45 @@
+package com.dabomstew.pkrandom.romhandlers.hack.ability.original;
+
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Gen5BattleEventType;
+import com.dabomstew.pkrandom.constants.ParagonLiteAbilities;
+import com.dabomstew.pkrandom.romhandlers.hack.AbilityHackMod;
+import com.dabomstew.pkrandom.romhandlers.hack.string.AbilityDescription;
+import com.dabomstew.pkrandom.romhandlers.hack.string.Dialogue;
+import com.dabomstew.pkrandom.romhandlers.hack.string.GameText;
+
+import java.util.List;
+
+public class AbilityHackMod_541_Veggie_Sword extends AbilityHackMod {
+    public AbilityHackMod_541_Veggie_Sword() {
+        super(ParagonLiteAbilities.veggieSword);
+    }
+
+    @Override
+    public String getName(Context context) {
+        return "Transistor";
+    }
+
+    @Override
+    public GameText getDescription(Context context) {
+        return new AbilityDescription(
+                "Powers up Grass-type",
+                "attacks."
+        );
+    }
+
+    @Override
+    public Dialogue getExplanation(Context context) {
+        return new Dialogue(
+                "Veggie Sword, huh...",
+                Dialogue.clearLine,
+                "This Ability increases the power of",
+                "Grass-type moves."
+        );
+    }
+
+    @Override
+    public void populateQueueEntries(Context context, List<QueueEntry> inOutQueueEntries) {
+        inOutQueueEntries.add(new QueueEntry(Gen5BattleEventType.onGetAttackingStatValue, "veggie_sword.s"));
+    }
+}
