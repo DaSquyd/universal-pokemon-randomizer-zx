bb05a5455666f2d875af1e3a683cb6f8ba1528fc
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2025-07-31 18:51:47 -0700

    Update contrary.s

--- src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/contrary.s
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/contrary.s
@@ -1,15 +1,17 @@
-    push    {r3-r5,LR}
-    mov     r0, #2
+    push    {r3-r6,LR}
+    mov     r6, r1
     mov     r5, r2
     mov     r4, r3
+    
+    mov     r0, #2
     bl      Battle::EventVar_GetValue
     cmp     r5, r0
     bne     Return
 
-    ; Ability Prompt
+    ; Popup Add
     mov     r0, r6
     mov     r1, #HE_AbilityPopup_Add
-    mov     r2, r7
+    mov     r2, r5
     bl      Battle::Handler_PushRun
 
     mov     r0, #VAR_Volume
@@ -20,5 +22,11 @@
     bl      Battle::EventVar_RewriteValue
     str     r0, [r4]
 
+    ; Popup Remove
+    mov     r0, r6
+    mov     r1, #HE_AbilityPopup_Remove
+    mov     r2, r5
+    bl      Battle::Handler_PushRun
+
 Return:
-    pop     {r3-r5,PC}
+    pop     {r3-r6,PC}
