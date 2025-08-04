4a0bd91eaf77df4658fabe1d3cf2080663e40269
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2025-08-04 11:58:38 -0700

    Update big_pecks.s

--- src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/big_pecks.s
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/ability/big_pecks.s
@@ -1,10 +1,7 @@
-#DEFINE EFFECT_CHANGE_STAT_STAGE 0x0E
-
-    push    {r3-r7,lr}
+    push    {r3-r5,lr}
     mov     r5, r1
     mov     r4, r2
 
-    mov     r6, #2
     mov     r0, #VAR_PokeId
     bl      Battle::EventVar_GetValue
     cmp     r4, r0
@@ -12,36 +9,35 @@
 
     mov     r0, #3
     bl      Battle::EventVar_GetValue
-    lsls    r0, r0, #0x18
-    lsrs    r1, r0, #0x18
+    mov     r1, r0
     mov     r0, r4
     bl      Battle::MainModule_IsAllyMonID
     cmp     r0, #0
     bne     Return
 
     mov     r0, #VAR_Volume
-    mov     r7, #0x20
     bl      Battle::EventVar_GetValue
     cmp     r0, #0
     bge     Return
 
     mov     r0, r5
-    mov     r1, #EFFECT_CHANGE_STAT_STAGE
+    mov     r1, #HE_ChangeStatStage
     mov     r2, r4
     bl      Battle::Handler_PushWork
     mov     r1, r0
-    ldr     r2, [r1]
-    lsls    r0, r7, #0x12
-    orrs    r0, r2
+    ldr     r2, [r1, #HandlerParam_ChangeStatStage.header]
+    mov     r0, #BHP_AbilityPopup
+    orr     r0, r2
     str     r0, [r1]
-    mov     r0, #STAT_Defense
-    str     r0, [r1,#4]
-    strb    r6, [r1,#0xC]
-    strb    r0, [r1,#0xE]
-    strb    r0, [r1,#0xF]
+    mov     r0, #STAT_Defense ; 2
+    str     r0, [r1,#HandlerParam_ChangeStatStage.stat] ; defense
+    strb    r0, [r1,#HandlerParam_ChangeStatStage.amount] ; 2
+    mov     r0, #1 ; true
+    strb    r0, [r1,#HandlerParam_ChangeStatStage.fAlways] ; true
+    strb    r0, [r1,#HandlerParam_ChangeStatStage.pokeCount] ; 1
     mov     r0, r5
-    strb    r4, [r1,#0x10]
+    strb    r4, [r1,#HandlerParam_ChangeStatStage.pokeId]
     bl      Battle::Handler_PopWork
 
 Return:
-    pop     {r3-r7,pc}
+    pop     {r3-r5,pc}
