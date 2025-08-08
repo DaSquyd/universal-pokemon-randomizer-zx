d3192e70d7d5c55361075c4bf8a2d2397f52325a
AphexCubed <therealnobodynoone@gmail.com>
2025-08-09 00:41:46 +0100

    Add Throat Spray + cleanup

--- src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/item/throat_spray.s
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/item/throat_spray.s
@@ -2,31 +2,35 @@
     mov     r6, r0
     mov     r5, r1
     mov     r4, r2
-    
+
+    ; get attacker
     mov     r0, #VAR_AttackingPoke
     bl      Battle::EventVar_GetValue
     cmp     r4, r0
     bne     Return
-    
-    ; TODO - do we need to explicitly check for miss here, or is this handled by the call like Tangled Feet?
-    
+
+    ; check if using a sound move
+    mov     r0, #VAR_MoveId
+    bl      Battle::EventVar_GetValue
+    lsl     r0, #16
+    lsr     r0, #16
+    mov     r1, #MF_Sound
+    bl      ARM9::MoveHasFlag
+    cmp     r0, #0
+    beq     Return
+
+    ; get Pokemon
     mov     r0, r5
     mov     r1, r4
     bl      Battle::GetPoke
     mov     r7, r0
-    
-    mov     r1, #BPV_SpeedStage
+
+    ; check if SpAtk can rise
+    mov     r1, #BPV_SpAtkStage
     mov     r2, #1
     bl      Battle::IsStatChangeValid
     cmp     r0, #0
     bne     PushRun
-    
-    mov     r0, r7
-    mov     r1, #BPV_AccuracyStage
-    mov     r2, #1
-    bl      Battle::IsStatChangeValid
-    cmp     r0, #0
-    beq     Return
     
 PushRun:
     mov     r0, r6
