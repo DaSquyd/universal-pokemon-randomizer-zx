bde29affae67d4dd649e1f02f904a4bd9b7398a3
AphexCubed <therealnobodynoone@gmail.com>
2025-08-09 12:48:51 +0100

    Add Room Service

---
+++ src/com/dabomstew/pkrandom/config/paragonlite/eventhandlers/item/room_service_drop_speed.s
@@ -0,0 +1,45 @@
+    push    {r3-r5, lr}
+    mov     r5, r1
+    mov     r4, r2
+    
+    mov     r0, #2
+    bl      Battle::EventVar_GetValue
+    cmp     r4, r0
+    bne     End
+    
+    mov     r0, r5
+    mov     r1, #HE_ChangeStatStage
+    mov     r2, r4
+    bl      Battle::Handler_PushWork
+    mov     r1, r0
+    
+    mov     r0, #STSG_Attack
+    str     r0, [r1, #0x04]
+    mov     r0, #2 ; boost amount
+    strb    r0, [r1, #0x0C]
+    mov     r0, #1
+    strb    r0, [r1, #0x0E]
+    strb    r0, [r1, #0x0F]
+    strb    r4, [r1, #0x10]
+    mov     r0, r5
+    bl      Battle::Handler_PopWork
+    
+    mov     r0, r5
+    mov     r1, #HE_ChangeStatStage
+    mov     r2, r4
+    bl      Battle::Handler_PushWork
+    mov     r1, r0
+    
+    mov     r0, #STSG_SpAtk
+    str     r0, [r1, #0x04]
+    mov     r0, #2 ; boost amount
+    strb    r0, [r1, #0x0C]
+    mov     r0, #1
+    strb    r0, [r1, #0x0E]
+    strb    r0, [r1, #0x0F]
+    strb    r4, [r1, #0x10]
+    mov     r0, r5
+    bl      Battle::Handler_PopWork
+    
+End:
+    pop     {r3-r5, pc}
